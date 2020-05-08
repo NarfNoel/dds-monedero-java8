c2c68bb3ea6b8a7fa7dcfa09535b1b9164419999
fleon <fleon@LAPTOP-UTMEM3T7>
2020-05-08 19:51:21 -0300

    Agrego Tests

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -1,6 +1,10 @@
 package dds.monedero.model;
 
 import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.time.LocalDate;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -61,5 +65,54 @@ public class MonederoTest {
   public void ExtraerMontoNegativo() {
     cuenta.sacar(-500);
   }
+  
+  @Test
+  public void Extraer() {
+	  cuenta.setSaldo(2000);
+	  cuenta.sacar(100);
+	  assertEquals(cuenta.getSaldo(), 1900, 0.01);
+  } 
+  
+  @Test
+  public void ExtraerTresVeces() {
+	  cuenta.setSaldo(2000);
+	  cuenta.sacar(100);
+	  cuenta.sacar(400);
+	  cuenta.sacar(500);
+	  assertEquals(cuenta.getSaldo(), 1000, 0.01);
+  }
+  
+  @Test
+  public void CrearCuentaYSetearMovimientos() {
+	  Cuenta cuentaNueva = new Cuenta(1000);
+
+	  assertEquals(cuentaNueva.getSaldo(), 1000, 0.01);
+  }
+  
+  @Test
+  public void SetearDepositos() {
+	  
+	  Deposito deposito1 = new Deposito(LocalDate.now(), 500);
+	  Deposito deposito2 = new Deposito(LocalDate.now(), 400);
+	  
+	  List<Deposito> depositos = Arrays.asList(deposito1,deposito2);
+	  cuenta.setDepositos(depositos);
+	  
+	  assertEquals(cuenta.getDepositos(), depositos);
+
+  }
+  
+  @Test
+  public void SetearExtracciones() {
+
+	  Extraccion extraccion1 = new Extraccion(LocalDate.now(), 200);
+	  Extraccion extraccion2 = new Extraccion(LocalDate.now(), 800);
+	  
+	  List<Extraccion> extracciones = Arrays.asList(extraccion1, extraccion2);
+
+	  cuenta.setExtracciones(extracciones);
+	  
+	  assertEquals(cuenta.getExtracciones(), extracciones);
+  }  
 
 }
